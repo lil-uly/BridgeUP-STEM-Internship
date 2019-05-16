73fdbb2ea8602917f2e5addd90599d4b788cce3b
Lily <ulysseliliann2021@gmail.com>
2019-05-16 17:24:23 -0400

    Made a diagram with SLA and Eddy contours on day 0

--- labs/eddy_contours_script_032819.py
+++ labs/eddy_contours_script_032819.py
@@ -138,22 +138,23 @@ def c(PATH):
     df.head
 
 
-#def closed(Path):
+def closed(Path):
   if coords[0] == coords[-1]:
       return True
 
-df[2].apply(True)
-
+df[0].apply(True)
+#Importing path1
 from matplotlib.path import Path
 ##the first path should return true
 path1 = Path([(295.18448575,  32.        ),(295.19526986,  32.2       ),(295.2       ,  32.20979566),(295.20711855,  32.2       ),(295.18448575,  32.        )], None)
 ##the second path should return false
 path2 = Path([(291.18448575,  22.        ),(295.19526986,  32.2       ),(295.2       ,  32.20979566),(295.20711855,  32.2       ),(295.23659369,  32.        )], None)
 
+#Vertices of the first path
 path1.vertices
 print(path1)
 
-c(path1)
+(path1)
 
 
 def closed(path):
@@ -175,20 +176,76 @@ df
 closed(path1)
 
 #Drops all of the false eddy values and creates a new dataset
-eddy_df = df[df.EDDY == True]
+eddy_df = df.reset_index(drop=True)
 eddy_df
-
 path1
 path1.vertices.shape
 path1.vertices[:,0]
 
 #Function to identify track all of the eddies
 def contourid(day):
-    lat = eddy_df.PATH.vertices[:,0]
-    lon = eddy_df.PATH.vertices[:,1]
-    cp = plt.contour(lon, lat, sla)
 
-contourid(2)
-Ppath1.vertices(lat)
+    lat = eddy_df.PATH[5].vertices[:,0]
+    lon = eddy_df.PATH[5].vertices[:,1]
+    plt.plot(lon, lat)
+    for i in (range(0,15)):
+        lat = df.PATH[i].vertices[:,0]
+        lon = df.PATH[i].vertices[:,1]
+        plt.plot(lon, lat)
+
+
+eddy_df.PATH[5].vertices[:,0]
+contourid(0)
+path1.vertices(lat)
 #if level[0] == level[-1]:
 #return True'''
+for i in (range(0,15)):
+    lat = df.PATH[i].vertices[:,0]
+    lon = df.PATH[i].vertices[:,1]
+    plt.plot(lon, lat)
+
+##Drops all open eddies
+closed_eddy_df = df[df.EDDY == True]
+
+##Resets the counter on the dataframe
+closed_eddy_df_2 = closed_eddy_df.reset_index(drop=True)
+closed_eddy_df_2
+
+## writing a function that checks a path against every other path (to see if it’s contained within those paths)
+
+def finding_inner(x):
+   counter = 0
+   for i in range(0, len(eddy_df_2.PATH)):
+       if closed_eddy_df_2.PATH[i].contains_path(x) == True and closed_eddy_df_2.PATH[i] != x:
+           counter+=1
+   if counter >= 1:
+       return "inner"
+   else:
+       return "outer"
+
+
+
+
+## applying the function to every path and adding the output to a new column
+closed_eddy_df_2["inner_or_outer"] = closed_eddy_df_2["PATH"].apply(finding_inner)
+
+## dropping inner eddies and saving to a new dataframe
+outer_eddy_df = closed_eddy_df_2[closed_eddy_df_2.inner_or_outer !=  "inner"]
+
+## resetting index after some rows were dropped
+outer_eddy_df = outer_eddy_df.reset_index(drop=True)
+
+outer_eddy_df.EDDY == True
+
+
+plt.figure()
+for i in (range(0,6)):
+    latitude = outer_eddy_df.PATH[i].vertices[:,1]
+    lon = outer_eddy_df.PATH[i].vertices[:,0]
+    plt.plot(lon, latitude)
+graph = plt.pcolor(long[:], lat[:], sla[day,:,:], cmap='cubehelix')
+cb = plt.colorbar(graph)
+cb.set_label('m')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Sea level Anomalies in Bermuda on day 0')
